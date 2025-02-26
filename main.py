8d147fdfc0469a9fa0357b68eff958c37f62fa0c
Bgeekprg <bhaveshpr54@gmail.com>
2025-02-26 15:43:50 +0530

    feat: category route added in main file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import Depends, FastAPI
 from database import Base, SessionLocal, engine
 import models
 from sqlalchemy.orm import Session
-from routes import auth
+from routes import auth, categories
 from dotenv import load_dotenv
 
 models.Base.metadata.create_all(bind=engine)
@@ -25,4 +25,4 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 
 app.include_router(auth.router)
-
+app.include_router(categories.router)
