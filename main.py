75d40ffcbf228e4d157984d30dfd059c23cb120c
Bgeekprg <bhaveshpr54@gmail.com>
2025-02-26 15:33:03 +0530

    feat: auth router added in main file.

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from typing import Annotated
 from fastapi import Depends, FastAPI
+from database import Base, SessionLocal, engine
+import models
 from sqlalchemy.orm import Session
+from routes import auth
 from dotenv import load_dotenv
 
-
+models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 load_dotenv()
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+app.include_router(auth.router)
 
