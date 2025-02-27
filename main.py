332a5afbc547c5476030a54bb3c0119d6249bf4e
Bgeekprg <bhaveshpr54@gmail.com>
2025-02-27 13:42:02 +0530

    feat: added products crud and localization

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
+import locale
 from typing import Annotated
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Request
 from database import Base, SessionLocal, engine
 import models
+from starlette.middleware.base import BaseHTTPMiddleware
 from sqlalchemy.orm import Session
 from routes import auth, categories, products
 from dotenv import load_dotenv
+import json
 
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 load_dotenv()
+
+
+class Localization:
+    def __init__(self, lang: str = "en"):
+        self.lang = self.normalize_language(lang)
+        self.messages = self.load_messages(self.lang)
+
+    def normalize_language(self, lang: str) -> str:
+        return lang.split("-")[0] if lang else "en"
+
+    def load_messages(self, lang: str) -> dict:
+        try:
+            with open(f"locales/{lang}.json", "r", encoding="utf-8") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            if lang != "en":
+                return self.load_messages("en")
+            raise HTTPException(
+                status_code=404, detail=f"Language file for {lang} not found."
+            )
+        except json.JSONDecodeError:
+            raise HTTPException(
+                status_code=500, detail="Failed to parse language file."
+            )
+
+    def gettext(self, message_key: str) -> str:
+        return self.messages.get(message_key, message_key)
+
+
+class LocalizationMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        lang = request.headers.get("Accept-Language", "ja")
+
+        localization = Localization(lang)
+        request.state.localization = localization
+
+        response = await call_next(request)
+        return response
 
 
 def get_db():
@@ -21,6 +62,7 @@ def get_db():
         db.close()
 
 
+app.add_middleware(LocalizationMiddleware)
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
--- routes/products.py
+++ routes/products.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import or_
 from database import SessionLocal
 from models import Product
+from routes.auth import get_current_user
 
 
 router = APIRouter(prefix="/products", tags=["Products"])
@@ -23,7 +24,7 @@ class ProductUpdate(BaseModel):
     id: int = Field(...)
     name: Optional[str] = None
     description: Optional[str] = None
-    stockQuantity: Optional[str] = None
+    stockQuantity: Optional[int] = None
     price: Optional[float] = None
     discount: Optional[int] = None
     categoryId: Optional[int] = None
@@ -39,7 +40,15 @@ def get_db():
         db.close()
 
 
+def admin_required(user):
+    if user["role"] != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Only admins can perform"
+        )
+
+
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 @router.get("/", status_code=status.HTTP_200_OK, response_model=List[ProductList])
@@ -88,17 +97,22 @@ async def get_products_list(
 
 
 @router.get("/products/{id}")
-async def get_product(id: int, db: db_dependency):
+async def get_product(id: int, db: db_dependency, request: Request):
+    localization = request.state.localization
+
     product = db.query(Product).filter(Product.id == id).first()
     if product:
         return product
     raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="Product is not available!"
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=localization.gettext("product_not_found"),
     )
 
 
 @router.post("/add", status_code=status.HTTP_201_CREATED)
 async def add_new_product(
+    request: Request,
+    user: user_dependency,
     db: db_dependency,
     product_name: str = Form(...),
     category_id: int = Form(...),
@@ -109,7 +123,10 @@ async def add_new_product(
     discount: int = Form(...),
     is_available: bool = Form(...),
 ):
+    localization = request.state.localization
     try:
+        admin_required(user)
+
         product = Product(
             name=product_name,
             categoryId=category_id,
@@ -123,58 +140,75 @@ async def add_new_product(
         db.add(product)
         db.commit()
         db.refresh(product)
-        return {"success": "product added Successfully"}
+        return {"success": localization.gettext("product_added_successfully")}
     except Exception as e:
         db.rollback()
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            # detail=f"Failed to add product: {str(e)}",
-            detail="Failed to add product: Check your data.",
+            detail=f"{localization.gettext('failed_to_add_product')}: {str(e)}",
         )
 
 
 @router.put("/update")
-async def update_product(db: db_dependency, product: ProductUpdate):
+async def update_product(
+    request: Request, db: db_dependency, product: ProductUpdate, user: user_dependency
+):
+    localization = request.state.localization
     db.begin()
     try:
+        admin_required(user)
 
-        if product.id:
-            data = db.query(Product).filter(Product.id == product.id).first()
-            if data:
+        data = db.query(Product).filter(Product.id == product.id).first()
+
+        if data:
+            if product.name != None:
                 data.name = product.name
+            if product.categoryId != None:
                 data.categoryId = product.categoryId
+            if product.productUrl != None:
                 data.productUrl = product.productUrl
+            if product.description != None:
                 data.description = product.description
+            if product.stockQuantity != None:
                 data.stockQuantity = product.stockQuantity
+            if product.price != None:
                 data.price = product.price
+            if product.discount != None:
                 data.discount = product.discount
+            if product.isAvailable != None:
                 data.isAvailable = product.isAvailable
-                db.add(data)
-                db.commit()
-                db.refresh(data)
-                return {"success": "product updated Successfully"}
-            else:
-                return {"error": "product not found"}
+
+            db.add(data)
+            db.commit()
+            db.refresh(data)
+            return {"success": localization.gettext("product_updated")}
+
         else:
             raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail="Product is not availbale"
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"{localization.gettext('failed_to_add_product')}: {str(e)}",
             )
+
     except Exception as e:
         db.rollback()
-        return {"error": f"{e}"}
+        return {"error": f"Failed to update product: {str(e)}"}
 
 
 @router.delete("/delete/{id}")
-async def delete_product(db: db_dependency, id: int):
+async def delete_product(
+    request: Request, db: db_dependency, id: int, user: user_dependency
+):
+    localization = request.state.localization
     db.begin()
     try:
+        admin_required(user)
         data = db.query(Product).filter(Product.id == id).first()
         if data:
             db.delete(data)
             db.commit()
-            return {"success": "product deleted Successfully"}
+            return {"success": localization.gettext("product_deleted")}
         else:
-            return {"error": "product not found"}
+            return {"error": localization.gettext("product_not_found")}
     except Exception as e:
         db.rollback()
-        return {"error": f"{e}"}
+        return {"error": localization.gettext(f"{e}")}
